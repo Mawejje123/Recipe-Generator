2dd81357bb7ee635559b5fe0a3bde39fd6233186
Eli Kayz <suneisiseli@gmail.com>
2026-01-21 13:16:11 +0300

    A simple demo of a Chef Claude App

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                
+                <span>Chef Claude</span>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
